bec4370a1ea3e9c9999cedc641ccfabfc9793918
Oleksandr Bobrov <bobrov@sheridancollege.ca>
2024-08-14 09:18:31 -0400

    Added contents to header and footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    DatePipe
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
 
+  name:string = "Oleksandr Bobrov";
+  today:Date = new Date();
 }
