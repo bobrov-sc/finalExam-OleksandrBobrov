e487763c1dbd3f93958d023d8b1d2d76d7615a24
Oleksandr Bobrov <bobrov@sheridancollege.ca>
2024-08-14 09:14:00 -0400

    Created project and generated main components

---
+++ src/app/college-details/college-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-college-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './college-details.component.html',
+  styleUrl: './college-details.component.css'
+})
+export class CollegeDetailsComponent {
+
+}
